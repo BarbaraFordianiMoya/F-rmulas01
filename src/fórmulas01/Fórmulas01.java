0d5aace5b9faf71a0651bc6ffc01edb583afaaed
Aluno <Aluno@10.85.198.10>
2024-06-05 14:39:23 -0300

    Fórmulas01

---
+++ src/fórmulas01/Fórmulas01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fórmulas01;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Fórmulas01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double t = 9*(33 - 32);
+        double x = 5/t;
+        System.out.println(x);
+    }
+    
+}
